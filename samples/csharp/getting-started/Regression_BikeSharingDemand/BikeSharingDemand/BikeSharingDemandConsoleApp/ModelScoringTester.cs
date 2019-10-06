468f08a9df072954f6c8f0df5e9f1999befffeb6
feiyun0112 <feiyun0112@gmail.com>
2019-10-06 13:30:18 +0800

    Regression_BikeSharingDemand 1.3.1

--- samples/csharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/ModelScoringTester.cs
+++ samples/csharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/ModelScoringTester.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
-using Microsoft.ML;
-using Microsoft.ML.Runtime.Api;
-using Microsoft.ML.Core.Data;
-using Microsoft.ML.Runtime;
-using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Runtime.Learners;
-
 using BikeSharingDemand.DataStructures;
-using Common;
+
+using Microsoft.ML;
 
 namespace BikeSharingDemand
 {
@@ -20,7 +14,7 @@ namespace BikeSharingDemand
         public static void VisualizeSomePredictions(MLContext mlContext,
                                                     string modelName, 
                                                     string testDataLocation,
-                                                    PredictionFunction<DemandObservation, DemandPrediction> predFunction,
+                                                    PredictionEngine<DemandObservation, DemandPrediction> predEngine,
                                                     int numberOfPredictions)
         {
             //Make a few prediction tests 
@@ -30,7 +24,7 @@ namespace BikeSharingDemand
             for (int i = 0; i < numberOfPredictions; i++)
             {
                 //Score
-                var resultprediction = predFunction.Predict(testData[i]);
+                var resultprediction = predEngine.Predict(testData[i]);
 
                 Common.ConsoleHelper.PrintRegressionPredictionVersusObserved(resultprediction.PredictedCount.ToString(), 
                                                             testData[i].Count.ToString());
@@ -47,19 +41,19 @@ namespace BikeSharingDemand
                 .Select(x => x.Split(','))
                 .Select(x => new DemandObservation()
                 {
-                    Season = float.Parse(x[2]),
-                    Year = float.Parse(x[3]),
-                    Month = float.Parse(x[4]),
-                    Hour = float.Parse(x[5]),
-                    Holiday = float.Parse(x[6]),
-                    Weekday = float.Parse(x[7]),
-                    WorkingDay = float.Parse(x[8]),
-                    Weather = float.Parse(x[9]),
-                    Temperature = float.Parse(x[10]),
-                    NormalizedTemperature = float.Parse(x[11]),
-                    Humidity = float.Parse(x[12]),
-                    Windspeed = float.Parse(x[13]),
-                    Count = float.Parse(x[16])
+                    Season = float.Parse(x[2], CultureInfo.InvariantCulture),
+                    Year = float.Parse(x[3], CultureInfo.InvariantCulture),
+                    Month = float.Parse(x[4], CultureInfo.InvariantCulture),
+                    Hour = float.Parse(x[5], CultureInfo.InvariantCulture),
+                    Holiday = float.Parse(x[6], CultureInfo.InvariantCulture),
+                    Weekday = float.Parse(x[7], CultureInfo.InvariantCulture),
+                    WorkingDay = float.Parse(x[8], CultureInfo.InvariantCulture),
+                    Weather = float.Parse(x[9], CultureInfo.InvariantCulture),
+                    Temperature = float.Parse(x[10], CultureInfo.InvariantCulture),
+                    NormalizedTemperature = float.Parse(x[11], CultureInfo.InvariantCulture),
+                    Humidity = float.Parse(x[12], CultureInfo.InvariantCulture),
+                    Windspeed = float.Parse(x[13], CultureInfo.InvariantCulture),
+                    Count = float.Parse(x[16], CultureInfo.InvariantCulture)
                 })
                 .Take(numberOfRecordsToRead)
                 .ToList();
